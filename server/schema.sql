51adbc5aa2d5c2b7c87b194d2ba2b2fbe2c7192b
Rakesh Ravi Shankar <kcrakeshravi@gmail.com>
2018-10-20 16:34:27 -0400

    Added schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,27 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS reserved;
 
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   fitbit_user_id INTEGER NOT NULL
+);
+
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT UNIQUE NOT NULL,
+  price FLOAT NOT NULL
+);
+
+CREATE TABLE reserved (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER NOT NULL,
+	product_id INTEGER NOT NULL,
+	amount_paid FLOAT NOT NULL,
+	last_step_count INTEGER NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
