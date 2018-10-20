4fd02db73cd235580e51da4dc7d576f575b71fc1
Bharat V <vaidhyanathan.b@husky.neu.edu>
2018-10-20 19:05:14 -0400

    Fitbit initial commit

---
+++ server/fitbit_wrapper/fitbitwrap.py
@@ -0,0 +1,23 @@
+import fitbit
+from . import gather_keys_oauth2 as Oauth2
+import pandas as pd 
+import datetime
+
+CLIENT_ID = '22D8NM'
+CLIENT_SECRET = 'bcca650d25f46953dcd2016a7d19b828'
+
+def main():
+	print("Hello Fitbit Wrap")
+
+def authorize():
+	server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+	server.browser_authorize()
+	ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+	REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+	auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+def getAverageSteps(fbuserID):
+	return 0
+
+if __name__ == '__main__':
+	authorize()
--- server/server.py
+++ server/server.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, Flask
+from .fitbit_wrapper import fitbitwrap 
 
 blueprint = Blueprint('server', __name__)
 
 @blueprint.route("/")
 def index():
     return "Welcome to Earned It!"
+
+@blueprint.route("/fitbittest")
+def fitbittest():
+	fitbitwrap.authorize()
+	return
